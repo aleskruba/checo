cb064c81b6c3d7d7ee4c9d8e3429869b53f4d0c4
Ales <aleskruba@seznam.cz>
2025-02-20 18:25:27 +0100

    change meta data

--- app/layout.tsx
+++ app/layout.tsx
@@ -50,24 +50,22 @@ export default function LocaleLayout({
 
         <meta
           name="description"
-          content="Portfolio of Aleš Kruba, a full-stack web developer specializing in frontend and backend development using JavaScript, React, Next.js, and Express.js."
-        />
+          content="Aprende checo de manera fácil y divertida con Aleš Kruba. Clases de gramática, pronunciación, diálogos prácticos y tests interactivos, especialmente para hispanohablantes."
+          />
         <meta name="author" content="Aleš Kruba" />
         <meta
           name="keywords"
-          content="Portfolio, Full Stack Web Development, JavaScript, TypeScript, React js, Next js, Express js, Tailwind CSS, Material UI, Docker, Databases, PHP, Python"
-        />
+          content="aprender checo, gramática checa, pronunciación checa, diálogos en checo, tests de checo, checo para hispanohablantes"
+          />
+
+        <meta property="og:title" content="Aprende Checo Fácilmente con Aleš | Gramática, Pronunciación, Diálogos" />
 
         <meta
-          property="og:title"
-          content="Aleš Kruba - Full Stack Web Developer Portfolio"
-        />
-        <meta
-          property="og:description"
-          content="Explore the portfolio of Aleš Kruba, showcasing my skills in frontend and backend web development, including React, Next.js, Express.js, and more. Projects and experiences displayed in a dynamic, user-friendly format."
-        />
-        <meta property="og:url" content="https://dokram.com" />
-        <meta property="og:image" content="https://dokram.com/man1.png" />
+            property="og:description"
+            content="Sumérgete en el idioma checo con cursos diseñados para hispanohablantes. Aprende gramática, mejora tu pronunciación y practica con diálogos reales."
+          />
+        <meta property="og:url" content="https://aprendercheco.com" />
+        <meta property="og:image" content="https://aprendercheco.com/man1.png" />
       </Head>
       <body
         suppressHydrationWarning
