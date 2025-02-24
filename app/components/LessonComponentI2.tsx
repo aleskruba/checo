e51189ad4a2877d187b4c7cb3e80761f3402eea4
Ales <aleskruba@seznam.cz>
2025-02-24 20:03:21 +0100

    principiantes 2 envII

--- app/components/LessonComponentI2.tsx
+++ app/components/LessonComponentI2.tsx
@@ -7,17 +7,8 @@ const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "http://localhost:3000";
 const items = {
   title: "Principiantes II ",
   content: [
-    { title: "Introducción", url: `${BASE_URL}/lesson/1` },
-    { title: "Números y palabras básicas", url: `${BASE_URL}/lesson/2` },
-    { title: 'El verbo "být"', url: `${BASE_URL}/lesson/3` },
-    { title: "Eso es + Vocabulario ", url: `${BASE_URL}/lesson/4` },
-    { title: "Caracteres especiales", url: `${BASE_URL}/lesson/5` },
-    { title: "Introducción a los adjetivos", url: `${BASE_URL}/lesson/6` },
-    { title: "Los posesivos básicos", url: `${BASE_URL}/lesson/7` },
-    { title: "Los plurales/sustantivos", url: `${BASE_URL}/lesson/8` },
-    { title: "Los plurales/sustantivos II", url: `${BASE_URL}/lesson/9` },
-    { title: 'El verbo "mít" + acusativo', url: `${BASE_URL}/lesson/10`},
-    { title: 'El verbo "vidět" + acusativo', url: `${BASE_URL}/lesson/11`},
+    { title: "Algo", url: `${BASE_URL}/lesson/1` },
+
     
   ],
 };
