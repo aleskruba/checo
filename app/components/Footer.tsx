b9c7f6a11af8a9bb0b97b3a0c28e9e6d8b8db63b
Ales <aleskruba@seznam.cz>
2025-03-29 17:10:09 +0100

    footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -11,22 +11,22 @@ function Footer() {
       <div className="container mx-auto px-4">
         <div className="space-y-2">
           <p className="text-sm">
-            <Link href="/privacy" className="text-blue-500 hover:text-blue-600 text-xs md:text-base">
+            <Link href="/privacy" className="dark:text-blue-300 dark:hover:text-blue-400 text-blue-800 hover:text-blue-900 text-xs md:text-base">
               Política de privacidad/Privacy Policy/Zásady ochrany osobních údajů
             </Link>
           </p>
           <p className="text-sm">
-            <Link href="/" className="text-blue-500 hover:text-blue-600 text-xs md:text-base">
+            <Link href="/" className="dark:text-blue-300 dark:hover:text-blue-400 text-blue-800 hover:text-blue-900 text-xs md:text-base">
               Sobre mi / About Me / O mně
             </Link>
           </p>
           <p className="text-sm">
-            <Link href="/contact" className="text-blue-500 hover:text-blue-600 text-xs md:text-base">
+            <Link href="/contact" className="dark:text-blue-300 dark:hover:text-blue-400 text-blue-800 hover:text-blue-900 text-xs md:text-base">
              Contacto / Contact /  Kontakt
             </Link>
           </p>
           <p className="text-sm mt-4">
-            &copy; {new Date().getFullYear()} <a href="https://aprendercheco.com" className="text-blue-500 hover:text-blue-600">aprendercheco.com</a>. All rights reserved.
+            &copy; {new Date().getFullYear()} <a href="https://aprendercheco.com" className="dark:text-blue-300 dark:hover:text-blue-400 text-blue-800 hover:text-blue-900 text-xs md:text-base">aprendercheco.com</a>. All rights reserved.
           </p>
         </div>
       </div>
