a06e14dc1843edb52683976a763ba41aa8a50f1f
Ales <aleskruba@seznam.cz>
2025-03-30 20:28:49 +0200

    clases youtube II

--- app/clases/page.tsx
+++ app/clases/page.tsx
@@ -30,7 +30,7 @@ const { isPlayingAll, stopAllAudio, isPlayingOne } = useAudio(); // Using contex
   return (
 
     <div className="max-w-4xl mx-auto p-4 space-y-6   ">
- <div className="w-[60%] max-w-3xl mx-auto">
+ <div className="md:w-[60%] w-[80%] max-w-3xl mx-auto">
       <div className="relative w-full aspect-video">
         <iframe
           className="absolute top-0 left-0 w-full h-full"
